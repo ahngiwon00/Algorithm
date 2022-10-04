ae68f9d8e1ca23a3581e74febd2bb8c6b795b226
ahngiwon00 <ahngiwon00@naver.com>
2022-10-04 17:56:23 +0900

    해시 폰켓몬

---
+++ src/HashSetPP.java
@@ -0,0 +1,12 @@
+public class HashSet {
+    public int solution(int[] nums) {
+        int answer = 0;
+        return answer;
+    }
+    public static void main(String[] args) {
+        HashSet t = new HashSet();
+        int[] nums = {3,3,3,2,2,4};
+        t.solution(nums);
+
+    }
+}
